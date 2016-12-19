c663feee7f2557e2840a6a62a21df4e9a848d033
LukaszKubiak <kubiak.lukasz1993@gmail.com>
2016-12-19 19:37:28 +0100

    Initial Commit

---
+++ Steganografia/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Steganografia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button7);
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button6);
+            this.splitContainer1.Panel2.Controls.Add(this.button5);
+            this.splitContainer1.Panel2.Controls.Add(this.button4);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Size = new System.Drawing.Size(953, 544);
+            this.splitContainer1.SplitterDistance = 470;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(178, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Szyfrowanie";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(159, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Odszyfrowywanie";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 397);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(20, 40);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(444, 397);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 454);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(443, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(20, 454);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(444, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 480);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Wczytaj obraz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(121, 480);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 38);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Zaszyfruj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(355, 481);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Zapisz obraz.";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(40, 480);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Załaduj obraz";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(194, 480);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 38);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Wczytaj obraz";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(344, 481);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 37);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Odszyfruj";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(225, 480);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(97, 38);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Wczytaj tekst z pliku";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 544);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Steganografia/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Steganografia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap bmp;
+        Bitmap bmp2;
+        string enc = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                bmp = new Bitmap(Bitmap.FromFile(openFileDialog1.FileName));
+                bmp = new Bitmap(bmp, pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = bmp;
+            }
+        }
+        public static byte[] ConvertToByteArray(string str, Encoding encoding)
+        {
+            return encoding.GetBytes(str);
+        }
+
+        public static String ToBinary(Byte[] data)
+        {
+            return string.Join(" ", data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (bmp != null)
+            {
+                bmp2 = bmp;
+                pictureBox2.Image = bmp2;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                bmp2 = new Bitmap(Bitmap.FromFile(openFileDialog1.FileName));
+                bmp2 = new Bitmap(bmp2, pictureBox2.Width, pictureBox2.Height);
+                pictureBox2.Image = bmp2;
+            }
+        }
+        void encode(int[] to_encode)
+        {
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width - 2; j++)
+                {
+                    if (j + i * Width >= to_encode.Length)
+                    {
+                        Color pixel1 = bmp.GetPixel(j, i);
+                        Color pixel3 = Color.FromArgb(pixel1.R - (pixel1.R % 8), pixel1.G - (pixel1.G % 8), pixel1.B - (pixel1.B % 8));
+                        bmp.SetPixel(j, i, pixel3);
+                        pixel1 = bmp.GetPixel(j + 1, i);
+                        pixel3 = Color.FromArgb(pixel1.R - (pixel1.R % 8), pixel1.G - (pixel1.G % 8), pixel1.B - (pixel1.B % 4));
+                        bmp.SetPixel(j + 1, i, pixel3);
+                        break;
+                    }
+                    int B = to_encode[j + i * Width] % 4;
+                    int G = ((to_encode[j + i * Width] % 32) - B) >> 2;
+                    int R = (to_encode[j + i * Width] - B - G) >> 5;
+                    Color pixel = bmp.GetPixel(j, i);
+                    Color pixel2 = Color.FromArgb(pixel.R - (pixel.R % 8) + R, pixel.G - (pixel.G % 8) + G, pixel.B - (pixel.B % 4) + B);
+                    int t = R * 32 + G * 4 + B;
+                    bmp.SetPixel(j, i, pixel2);
+
+                }
+
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (bmp != null)
+            {
+                if (enc == "")
+                {
+                    int[] to_encode = Encoding.ASCII.GetBytes(textBox1.Text).Select(x => (int)x).ToArray();
+                    encode(to_encode);
+                    pictureBox1.Refresh();
+                }
+                else
+                {
+                    int[] to_encode = Encoding.ASCII.GetBytes(enc).Select(x => (int)x).ToArray();
+                    encode(to_encode);
+                    pictureBox1.Refresh();
+                }
+                
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (bmp2 != null)
+            {
+                string Encoded = "";
+
+                for (int i = 0; i < bmp2.Height; i++)
+                {
+                    for (int j = 0; j < bmp2.Width - 2; j++)
+                    {
+                        Color pixel = bmp2.GetPixel(j, i);
+                        int RGB = (pixel.R%8) * 32 + (pixel.G%8) * 4 + (pixel.B%4);
+                        if (RGB == 0)
+                        {
+                            Color pixel2 = bmp2.GetPixel(j+1, i);
+                            int RGB2 = (pixel2.R%8) * 32 + (pixel2.G%8) * 4 + (pixel2.B%4);
+                            if (RGB2 == 0)
+                            {
+                                i = bmp2.Height;
+                                break;
+                            }
+
+                        }
+                        Encoded += (char)RGB;
+                    }
+
+                }
+                if (Encoded.Length <= textBox2.MaxLength)
+                    textBox2.Text = Encoded;
+                else
+                {
+                    saveFileDialog1.DefaultExt = ".txt";
+            DialogResult dr = saveFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                StreamWriter sr = new StreamWriter(saveFileDialog1.FileName);
+                sr.Write(Encoded);
+
+            }
+
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.DefaultExt = ".jpg";
+            DialogResult dr = saveFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                bmp.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+             openFileDialog1.Filter = "Text (*.txt) | *.txt";
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                if (bmp != null)
+                {
+                   
+                    foreach(var line in File.ReadAllLines(openFileDialog1.FileName))
+                    {
+                        enc +=line;
+                    }
+                   
+
+                }
+
+            }
+        }
+    }
+}
